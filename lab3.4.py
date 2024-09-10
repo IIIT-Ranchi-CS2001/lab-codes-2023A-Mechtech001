95529af1d229a61db6d1ff55e531bc5d59597b9c
Aditya Sharma <aditya16may@gmail.com>
2024-09-10 12:57:36 +0530

    lab 3 commit

--- lab3.4.py
+++ lab3.4.py
@@ -1,4 +1,4 @@
-num = int(input("Enter the number for the multiplication table: "))  
+num = int(input("Enter the number for the multiplication table : "))  
 limit = int(input("Enter the limit: "))
 print(f"Multiplication table for {num} up to {limit}:")
 for i in range(1, limit + 1):
